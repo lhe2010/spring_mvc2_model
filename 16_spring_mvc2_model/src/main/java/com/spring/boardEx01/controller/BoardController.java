5835286aabc564aad2c97ec66740f692e6725a84
15시_웹개발_LHE <15__LHE@DESKTOP-LDTTVBK>
2021-01-27 15:16:51 +0900

    2021-01-27 수 Controller 수정

--- 16_spring_mvc2_model/src/main/java/com/spring/boardEx01/controller/BoardController.java
+++ 16_spring_mvc2_model/src/main/java/com/spring/boardEx01/controller/BoardController.java
@@ -12,6 +12,6 @@ public class BoardController {
 	// value에는 url 주소를 명시, method는 요청타입을 명시 (이 url에 get으로 접근시 리턴값의 jsp 파일로 보내줌)
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String main() {			// 별로 메소드명은 중요하지 않음
-		return "boardEx01/01_main";	// servlet-context에 명시된 대로(prefix와 suffix) 포워딩 시킬 jsp파일을 작성해준다.
+		return "boardEx01/main";	// servlet-context에 명시된 대로(prefix와 suffix) 포워딩 시킬 jsp파일을 작성해준다.
 	}
 }
